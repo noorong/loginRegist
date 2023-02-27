282190f3d0c76179006b05fec9deffc3885a5a9f
noorong <100012522+noorong@users.noreply.github.com>
2023-02-27 16:38:40 +0900

    feat: privateRouting 개발완료

--- src/components/PrivateRouting.jsx
+++ src/components/PrivateRouting.jsx
@@ -1,21 +1,7 @@
-import { Route, redirect, useLocation } from "react-router-dom";
+import { redirect, useLocation } from "react-router-dom";
 
-export default function PrivateRoute({ children, ...props }) {
+export default function PrivateRoute({ children }) {
   const location = useLocation();
-  console.log(props);
-  return (
-    <Route
-      {...props}
-      render={() => {
-        // location.state가 없으면, /login 페이지로 이동합니다.
-        const isLoggedIn = !!location.state.user;
 
-        if (!isLoggedIn) {
-          return redirect("/login");
-        }
-
-        return children;
-      }}
-    />
-  );
+  return !!location.state.user ? children : redirect("/login");
 }
